92ddb6c01ea336fed518d1fe736e59d2ae7c6979
Martin Horvat <martinhorpriv@gmail.com>
2022-10-21 17:42:48 +0200

    Added read from memory in C++

--- RedPitaya_server/main.cpp
+++ RedPitaya_server/main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
-#include <memory>
-#include "uio_parser.h"
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 using namespace std;
 
-int main(){
-    auto uioList = uio_lib::GetUioList();
+int main() {
+    cout << sysconf(_SC_PAGE_SIZE) << endl;
 
-    cout << uioList.size() << endl;
+    int fd = ::open("/dev/mem", O_RDONLY);
+    int *map = NULL;
+
+    if (fd < 0) {
+		cout << "Failed to open file!" << endl;
+
+        return -1;
+	}
+	else {
+		cout << "Successfully opened file!" << endl;
+    }
+
+    map = (int *)(mmap(NULL, 8, PROT_READ, MAP_SHARED, fd, 0x19000000));
+
+    int N = 10;
+
+    for (int i=0; i<N; i++) {
+        cout << *map << endl;
+        sleep(1);
+    }
+
+    munmap(map, 8);
+    ::close(fd);
 
     return 0;
 }
